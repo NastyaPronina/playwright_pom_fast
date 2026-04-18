b5cbf65c619ecb9b3041801e53d85492b494a2a3
Anastasiya <nastyapro876@gmail.com>
2026-04-18 12:00:56 +0300

    add remove item logic and test

--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -8,6 +8,8 @@ class InventoryPage(BasePage):
         self._first_item_add_button = page.locator("[data-test^='add-to-cart']").first
         # Локатор иконки корзины
         self._cart_badge = page.locator("[data-test='shopping-cart-badge']")
+        # Локатор для кнокпки Remove
+        self._remove_button = page.locator("[data-test='remove-sauce-labs-backpack']")
 
     @allure.step("Добавление первого товара в корзину")
     def add_first_item_to_cart(self):
@@ -16,3 +18,11 @@ class InventoryPage(BasePage):
     @allure.step("Получаем количество товара в корзине")
     def get_cart_count(self):
         return self._cart_badge.inner_text()
+
+    @allure.step("Удаление товара из корзины через страницу каталога")
+    def remove_first_item(self):
+        self._remove_button.click()
+
+    def is_remove_button_visible(self):
+        # Проверяем, видна ли кнопка удаления (возвращает True/False)
+        return self._remove_button.is_visible()
--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -1,21 +1,35 @@
 from playwright.sync_api import Page, expect
+import allure
 
 
+@allure.title("Тест на добавление первого в списке товара в корзину")
 def test_add_item_to_cart(logged_in_page):
     logged_in_page.add_first_item_to_cart()
-    assert logged_in_page.get_cart_count() == "1"
+    expect(logged_in_page._cart_badge).to_have_text("1")
     print("\nТовар успешно добавлен, в корзине 1 предмет!")
 
-
+@allure.title("Тест на наличие товара в корзине")
 def test_item_in_cart(logged_in_page, cart_page):
     logged_in_page.add_first_item_to_cart()
     cart_page.open("https://www.saucedemo.com/cart.html")
-    assert cart_page.get_cart_item_name() == "Sauce Labs Backpack"
+    expect(cart_page._cart_item_name).to_have_text("Sauce Labs Backpack")
 
+@allure.title("Тест на совершение заказа товара")
 def test_full_checkout_flow(logged_in_page, cart_page, checkout_page):
     logged_in_page.add_first_item_to_cart()
     cart_page.open("https://www.saucedemo.com/cart.html")
     cart_page.click_checkout()
     checkout_page.fill_checkout_form("Ann", "Parker", "123456")
     checkout_page.finish_order()
-    assert checkout_page.get_success_message() == "Thank you for your order!"
+    expect(checkout_page._success_message).to_have_text("Thank you for your order!")
+
+@allure.title("Тест на добавление и последующее удаление товара")
+def test_add_and_remove_item(logged_in_page):
+    logged_in_page.add_first_item_to_cart()
+    # Проверяем, что кнопка Remove видна
+    expect(logged_in_page._remove_button).to_be_visible()
+    logged_in_page.remove_first_item()
+    # Проверяем, что т.к. корзина пустая, то элемент 'shopping-cart-badge' отсутствует в DOM
+    expect(logged_in_page._cart_badge).not_to_be_attached()
+
+
